552cf983ec7a04bf93accea45e49d1c67297ca6f
PHYLLIPE GABRYEL CARDOSO DE OLIVEIRA <01412106@sempreuninabuco.com.br>
2023-09-08 21:37:03 -0300

    última att 08/09 POO

---
+++ Aulajava/Exercícios.java
@@ -0,0 +1,33 @@
+package Aulajava;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercícios {
+    public static void main(String[] args) {
+        /*Pilhas são elementos onde se tem base a partir do último elemento adicionado, sendo possível alterar, adicionar e remover.
+         diferente de Filas, onde o elemento a ser alterado é o primeiro que foi adicionado.
+         E listas abrangem uma variedade maior, sendo possível adicionar, remover e alterar elementos de qualquer posição.
+          */
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<>();
+        int num = 1;
+
+        for(int i = 0; i < 4; i++){
+            System.out.print("Insira um valor: ");
+            num = Integer.parseInt(scan.nextLine());
+            lista.add(num);
+            System.out.println(lista);  
+        }
+        for (int i = 0; i < 4; i++){
+            lista.remove(0);
+        }
+        if(lista.isEmpty()){
+            System.out.println("A lista está vazia");
+        }else{
+            System.out.println("A lista não está vazia");
+        }
+    }
+        
+    }    
+
